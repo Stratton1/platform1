ce8e0a9bbca6d1bec3f304cf5fbc68553c4b79c1
Stratton1 <joe.b.weaver@googlemail.com>
2024-10-29 21:50:22 +0000

    Move pages to correct directory and prepare for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Specify that pages and components are in the src directory
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-};
-
-export default nextConfig;
+ 
